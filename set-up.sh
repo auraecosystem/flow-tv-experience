bc01e3c158d8d93f187fdbe355f0abcf1240dacf
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-09-13 23:00:48 +0100

    Create set-up.sh

---
+++ set-up.sh
@@ -0,0 +1,6 @@
+# 1. Clone the repository
+git clone https://github.com/auraecosystem/flow-tv-experience.git
+# 2. Navigate and start local server
+cd flow-tv-experience && npm start
+# 3. Open browser URL
+http://localhost:3000
