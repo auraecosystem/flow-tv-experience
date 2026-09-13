499ab88c1e4fc1daf6d33773f7b8756c349acb88
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-09-13 01:05:23 +0100

    Update clone.sh with flow-tv-experience setup
    Add instructions to clone and start flow-tv-experience

--- clone.sh
+++ clone.sh
@@ -3,3 +3,7 @@ cd gods-eye-view
 npm ci
 npm run doctor
 npm run dev
+man Clone-git https://github.com/auraecosystem/flow-tv-experience.git
+cd flow-tv-experience
+Start the local server:
+npm start
