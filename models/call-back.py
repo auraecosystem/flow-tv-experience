4d3cf9b0cc659abaedc74bf3dfe1696dbabf35ae
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-09-13 01:47:55 +0100

    Add track_cost_callback for response cost tracking
    Implement a callback to track response cost during completion.

---
+++ models/call-back.py
@@ -0,0 +1,27 @@
+import litellm
+
+# track_cost_callback
+def track_cost_callback(
+    kwargs,                 # kwargs to completion
+    completion_response,    # response from completion
+    start_time, end_time    # start/end time
+):
+    try:
+      response_cost = kwargs.get("response_cost", 0)
+      print("streaming response_cost", response_cost)
+    except:
+        pass
+# set callback
+litellm.success_callback = [track_cost_callback] # set custom callback function
+
+# litellm.completion() call
+response = completion(
+    model="openai/gpt-5",
+    messages=[
+        {
+            "role": "user",
+            "content": "Hi 👋 - i'm openai"
+        }
+    ],
+    stream=True
+)
